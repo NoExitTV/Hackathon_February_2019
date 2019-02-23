2b215a98421ee216d09218050278b674766cfe0d
phnk <carl.borngrund@gmail.com>
2019-02-23 06:34:05 +0100

    lel

--- rectangular-benchmark.py
+++ rectangular-benchmark.py
@@ -325,7 +325,7 @@ for model_name in models_list:
     criterion = nn.CrossEntropyLoss()
 
     # Train and evaluate
-    model_ft, hist = train_model(model_ft, dataloaders_dict, criterion, optimizer_ft, num_epochs=num_epochs, is_inception=(model_name=="inception"))
+    # model_ft, hist = train_model(model_ft, dataloaders_dict, criterion, optimizer_ft, num_epochs=num_epochs, is_inception=(model_name=="inception"))
 
     # Save model
     torch.save(model_ft.state_dict(), "./saved-models/"+model_name)
