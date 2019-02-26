123883e0a8ecfea49ddfbf8fef57e9f7be3dc82a
NoExitTV <fredrikopettersson@hotmail.com>
2019-02-26 13:45:09 +0100

    Got really good results! 78.3%! Noticed that I did not test on original test split. Try with that now

--- square-benchmark.py
+++ square-benchmark.py
@@ -163,52 +163,73 @@ def initialize_model(model_name, num_classes, feature_extract, use_pretrained=Tr
     model_ft = None
     input_size = 0
 
-    if model_name == "resnet":
-        """ Pretrained resnet
+    if model_name == "resnet18":
+        """ Resnet18
         """
-        model_ft = models.resnet18(pretrained=False)
-        checkpoint = torch.load('./pretrained-models/resnet/model_best.pth.tar')
-        state_dict = {str.replace(k, 'module.', ''): v for k, v in checkpoint[
-                    'state_dict'].items()}
-        model_ft.load_state_dict(state_dict)
-        model_ft.eval()
+        model_ft = models.resnet18(pretrained=use_pretrained)
         set_parameter_requires_grad(model_ft, feature_extract)
-        # num_ftrs = model_ft.fc.in_features
-        # model_ft.fc = nn.Linear(64512, num_classes)
-        
-        # Change last layer
-        model_ft.fc = nn.Sequential(
-            nn.Dropout(p=0.1),
-            nn.Linear(64512, 4096),
-            nn.Linear(4096, num_classes)
-        ) 
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
 
+    elif model_name == "resnet50":
+        """ Resnet18
+        """
+        model_ft = models.resnet50(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
 
     elif model_name == "alexnet":
-        """ Pretrained alexnet
+        """ Alexnet
         """
-        model_ft = AlexNet()
-        checkpoint = torch.load('./pretrained-models/alexnet/model_best.pth.tar')
-        state_dict = {str.replace(k, 'module.', ''): v for k, v in checkpoint[
-                    'state_dict'].items()}
-        model_ft.load_state_dict(state_dict)
-        model_ft.eval()
+        model_ft = models.alexnet(pretrained=use_pretrained)
         set_parameter_requires_grad(model_ft, feature_extract)
         num_ftrs = model_ft.classifier[6].in_features
-        model_ft.classifier[6] = nn.Linear(num_ftrs, num_classes)
+        model_ft.classifier[6] = nn.Linear(num_ftrs,num_classes)
+        input_size = 224
 
     elif model_name == "vgg":
-        """ Pretrained vgg
+        """ VGG11_bn
         """
-        model_ft = vgg13()
-        checkpoint = torch.load('./pretrained-models/vgg/model_best.pth.tar')
-        state_dict = {str.replace(k, 'module.', ''): v for k, v in checkpoint[
-                    'state_dict'].items()}
-        model_ft.load_state_dict(state_dict)
-        model_ft.eval()
+        model_ft = models.vgg11_bn(pretrained=use_pretrained)
         set_parameter_requires_grad(model_ft, feature_extract)
         num_ftrs = model_ft.classifier[6].in_features
-        model_ft.classifier[6] = nn.Linear(num_ftrs, num_classes)
+        model_ft.classifier[6] = nn.Linear(num_ftrs,num_classes)
+        input_size = 224
+
+    elif model_name == "squeezenet":
+        """ Squeezenet
+        """
+        model_ft = models.squeezenet1_0(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        model_ft.classifier[1] = nn.Conv2d(512, num_classes, kernel_size=(1,1), stride=(1,1))
+        model_ft.num_classes = num_classes
+        input_size = 224
+
+    elif model_name == "densenet":
+        """ Densenet
+        """
+        model_ft = models.densenet121(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier.in_features
+        model_ft.classifier = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    elif model_name == "inception":
+        """ Inception v3
+        Be careful, expects (299,299) sized images and has auxiliary output
+        """
+        model_ft = models.inception_v3(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        # Handle the auxilary net
+        num_ftrs = model_ft.AuxLogits.fc.in_features
+        model_ft.AuxLogits.fc = nn.Linear(num_ftrs, num_classes)
+        # Handle the primary net
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs,num_classes)
+        input_size = 299
 
     else:
         print("Invalid model name, exiting...")
@@ -217,17 +238,17 @@ def initialize_model(model_name, num_classes, feature_extract, use_pretrained=Tr
     return model_ft, input_size
 
 def load_data(input_size, batch_size):
-    target_resolution = (480, 640)
+    target_resolution = (input_size, input_size)
 
     print("Initializing Datasets and Dataloaders...")
 
-    resize_and_crop = torchvision.transforms.Compose([torchvision.transforms.Resize((720, 960)),
+    resize_and_crop = torchvision.transforms.Compose([torchvision.transforms.Resize((input_size*1.5, input_size*1.5)),
                                             torchvision.transforms.RandomCrop(target_resolution)])
 
     transform_train = torchvision.transforms.Compose([torchvision.transforms.RandomChoice([torchvision.transforms.Resize(target_resolution), resize_and_crop]),
                                            transforms.RandomHorizontalFlip(),
-                                           torchvision.transforms.RandomRotation((-15,15), resample=False, expand=False, center=None),
-                                           #torchvision.transforms.RandomVerticalFlip(),
+                                           torchvision.transforms.RandomRotation((-20,20), resample=False, expand=False, center=None),
+                                           torchvision.transforms.RandomVerticalFlip(),
                                            torchvision.transforms.ToTensor(),
                                            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
@@ -285,8 +306,8 @@ if torch.cuda.is_available():
     print("torch.cuda.get_device_name(0)", torch.cuda.get_device_name(0))
 
 batch_size = 16 # Minibatch size
-num_epochs = 75
-learning_rate = 0.5e-3
+num_epochs = 25
+learning_rate = 1e-3
 num_classes = 10
 
 
@@ -294,20 +315,21 @@ num_classes = 10
 ########## Run tests ##########
 
 #models_list = ["resnet", "alexnet", "vgg"]
-models_list = ["resnet"]
+models_list = ["resnet50"]
 results = []
-
-train_loader, val_loader, test_loader, classes = load_data(0, batch_size)
-dataloaders_dict = {"train": train_loader, "test": test_loader, "val": val_loader}
 
 for model_name in models_list:
 
     # Flag for feature extracting. When False, we finetune the whole model,
     #   when True we only update the reshaped layer params
-    feature_extract = False
+    feature_extract = True
 
     # Initialize the model for this run
     model_ft, input_size = initialize_model(model_name, num_classes, feature_extract, use_pretrained=True)
+
+    # initialize dataset
+    train_loader, val_loader, test_loader, classes = load_data(input_size, batch_size)
+    dataloaders_dict = {"train": train_loader, "test": test_loader, "val": val_loader}
 
     # Print the model we just instantiated
     print(model_ft)
