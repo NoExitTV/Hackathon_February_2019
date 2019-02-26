9c4d491a673dd3e2e225804fe53841b5d07fefd0
NoExitTV <fredrikopettersson@hotmail.com>
2019-02-26 15:57:18 +0100

    Some renaming

--- test-resnet.py
+++ test-resnet.py
@@ -6,7 +6,6 @@ import torchvision
 from torchvision import datasets, models, transforms
 import torch.optim as optim
 import numpy as np
-import matplotlib.pyplot as plt
 import time
 import os
 import copy
